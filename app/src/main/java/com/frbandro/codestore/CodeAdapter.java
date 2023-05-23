ff590f26f7685becb4c80630d37dafc6885e296a
Akshay Jangid <akshayjangid703@gmail.com>
2023-05-23 18:32:30 +0530

    edit recycler view

--- app/src/main/java/com/frbandro/codestore/CodeAdapter.java
+++ app/src/main/java/com/frbandro/codestore/CodeAdapter.java
@@ -1,12 +1,54 @@
 package com.frbandro.codestore;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CodeAdapter extends {
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class CodeAdapter extends FirebaseRecyclerAdapter<CodeModel,CodeAdapter.CodeViewHolder> {
 
 
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public CodeAdapter(@NonNull FirebaseRecyclerOptions<CodeModel> options) {
+        super(options);
+    }
 
+    @Override
+    protected void onBindViewHolder(@NonNull CodeAdapter.CodeViewHolder holder, int position, @NonNull CodeModel model) {
+
+        holder.a.setText(model.getXmlcode());
+        holder.b.setText(model.getXmlcode());
+    }
+
+    @NonNull
+    @Override
+    public CodeAdapter.CodeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view
+
+                = LayoutInflater.from(parent.getContext())
+
+                .inflate(R.layout.show_data_rv, parent, false);
+
+        return new CodeAdapter.CodeViewHolder(view);
+    }
+
+    public class CodeViewHolder extends RecyclerView.ViewHolder {
+        TextView a,b;
+        public CodeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            a=itemView.findViewById(R.id.codexml);
+            b=itemView.findViewById(R.id.codejava);
+        }
+    }
 }
--- app/src/main/java/com/frbandro/codestore/MainActivity.java
+++ app/src/main/java/com/frbandro/codestore/MainActivity.java
@@ -1,14 +1,74 @@
 package com.frbandro.codestore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseReference connect;
+    RecyclerView recy;
+    CodeAdapter cadtr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connect=FirebaseDatabase.getInstance().getReference();
+
+        recy = findViewById(R.id.code_holder);
+
+        // To display the Recycler view linearly
+
+        recy.setLayoutManager(
+
+                new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<CodeModel> options
+
+                = new FirebaseRecyclerOptions.Builder<CodeModel>()
+
+                .setQuery(connect,CodeModel.class).build();
+
+        // Connecting object of required Adapter class to
+
+        // the Adapter class itself
+
+        cadtr = new CodeAdapter(options);
+
+        // Connecting Adapter class with the Recycler view*/
+
+        recy.setAdapter(cadtr);
+    }
+
+    @Override protected void onStart()
+
+    {
+
+        super.onStart();
+
+        cadtr.startListening();
+
+    }
+
+
+
+    // Function to tell the app to stop getting
+
+    // data from database on stopping of the activity
+
+    @Override protected void onStop()
+
+    {
+
+        super.onStop();
+
+        cadtr.stopListening();
+
     }
 }
