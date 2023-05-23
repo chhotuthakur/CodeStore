010038326cc03eea73f1374faeee75367f80e2e9
chhotuthakur <npsdahiya@gmail.com>
2023-05-23 17:12:44 +0530

    added firebase UI

--- app/src/main/java/com/frbandro/codestore/CodeAdapter.java
+++ app/src/main/java/com/frbandro/codestore/CodeAdapter.java
@@ -5,23 +5,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CodeAdapter extends RecyclerView.Adapter<CodeModel,CodeAdapter.ViewHolder> {
+public class CodeAdapter extends {
 
-    @NonNull
-    @Override
-    public CodeModel onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
 
-    @Override
-    public void onBindViewHolder(@NonNull CodeModel holder, int position) {
 
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-
-   
 }
